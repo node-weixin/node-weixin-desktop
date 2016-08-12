3fac1c230af7d3d887a0f16752cf865d045ec6d9
calidion <calidion@gmail.com>
2016-08-12 18:25:17 +0800

    updated

---
+++ install.sh
@@ -0,0 +1,6 @@
+#npm install -g electron-prebuilt
+#npm install -g electron-packager
+export ELECTRON_MIRROR="https://npm.taobao.org/mirrors/electron/"
+export SASS_BINARY_SITE="https://npm.taobao.org/mirrors/node-sass"
+npm install
+
